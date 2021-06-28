e65f7d74e5df5ef3d87476748ab794197fa66798
Ali Hadi Öztürk <alihadi06@gmail.com>
2021-06-28 12:13:35 +0300

    google analytics deleted && google tag manager added

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ReactGA from 'react-ga';
 import App from './App';
 import { ContextWrapper } from './core/AppContext';
 import './index.css';
@@ -15,8 +14,6 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-ReactGA.initialize('G-32DS6SDHD5');
-ReactGA.pageview(window.location.pathname + window.location.search);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
